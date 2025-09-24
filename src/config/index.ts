6d6a58b7567115625c134800fdbe09a90a732738
dioniks12 <dionilodrigus12@gmail.com>
2025-09-24 14:16:53 +0700

    first commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,21 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Dioni Krisna Saputra Lodrigus — Software Developer",
+  author: "Dioni Krisna Saputra Lodrigus",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    "Software Developer based in Madiun City, East Java. I specialize in Web Development using Laravel, and also have Mobile Development using Flutter.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/dion-small.jpg",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
-    { text: "About", href: "#about" },
+    { text: "About Me", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/dioni-lodrigus" },
+    { text: "Github", href: "https://github.com/dioniks12" },
+    { text: "Instagram", href: "https://github.com/immois/astro-zen" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -25,73 +23,52 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Dioni Krisna Saputra Lodrigus",
+    specialty: "Web Developer",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+      "Developer based in Madiun City, East Java. I specialize in Web Development using Laravel, MySQL, PostgreSQL, additionally, I have experience in Mobile Development using Flutter.",
+    email: "dionilodrigus12@email.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "Dinas Komunikasi dan Informatika Kota Madiun",
+      position: "Software Developer Internship",
+      startDate: "Jan 2025",
+      endDate: "Feb 2025",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Succesfully finished internship project at Dinas Komunikasi dan Informatika Kota Madiun, and also integrating Laravel, PostgreSQL, and RTSP to provide live CCTV in every locations.",
       ],
-    },
-    {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
-      summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
-      ],
-    },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
     },
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/spotifu.png",
+      name: "Quick Fresh Laundry",
+      summary: "Successfully completed the Quick Fresh Laundry project, a major assignment for the Web Programming course in my 5th semester. Working collaboratively in a team, implemented a user-friendly UI by adhering to best practices in design. The project utilized Laravel for the backend development and MySQL as the database. Additionally, we integrated RESTful API's for efficient data management and seamless communication between the frontend and backend systems.",
+      image: "/laundry.png",
     },
     {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/shopify-clon.png",
+      name: "Barbershop App",
+      summary: "Completed a salon-themed Android application project using Flutter, as part of a collaborative team effort. Implemented a user-friendly UI by following best design practices and integrated REST APIs for efficient data management. Firebase was utilized for data storage, ensuring seamless and secure handling of user information and application data",
+      image: "/barbershop.png",
     },
     {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/clone-ig.png",
+      name: "Atma Kitchen Bakery & Sales",
+      summary: "Sucessfully finished web based project on 6th semester, Implementing Laravel framework and MySQL databases about bakery and order topics",
+      image: "/atma-kitchen.png",
+    },
+    {
+      name: "Financial and Goods Management System For UMKM Toko UUS Kanigoro",
+      summary: "Successfully finished a Final Assignment Project about Financial and Goods Management for Toko UUS Kanigoro, to helps the owner to maintain the financial and support the decision making for her business.",
+      image: "/toko-uus.png",
     },
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
+      Hi, I’m Dioni Krisna Saputra Lodrigus, A Fresh graduated and passionate Web Developer with a talent for crafting seamless digital experiences. With a strong background in both Android and iOS development, along with expertise in front-end web technologies, I thrive at the intersection where creativity meets technology.
 
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+      Over the years, I've honed my skills in building robust, user-friendly applications that not only meet user needs but also push the boundaries of what's possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/dion-big.jpg",
   },
 };
 
